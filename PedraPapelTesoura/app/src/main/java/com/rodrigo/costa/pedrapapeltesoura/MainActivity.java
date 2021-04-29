ca63f0673956a33dfd1cc9ba550ce5bb499f5135
rodrigosouzacosta89 <rodrigosouzacosta89@gmail.com>
2021-04-29 10:13:29 -0300

    teste

---
+++ PedraPapelTesoura/app/src/main/java/com/rodrigo/costa/pedrapapeltesoura/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rodrigo.costa.pedrapapeltesoura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
